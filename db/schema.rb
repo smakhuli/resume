6110ce4b549fd73d0a13893d2adf0f8b2e43cb04
Sami Makhuli <sami.makhuli@ibethel.org>
2018-10-04 04:05:38 -0700

    converted db from sqlite3 to Postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_04_075726) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "profiles", force: :cascade do |t|
     t.string "address1"
     t.string "address2"
