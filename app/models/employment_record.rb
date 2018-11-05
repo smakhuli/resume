97f90c4575cc9de5f47129fd360d1213f598913a
Sami Makhuli <sami.makhuli@ibethel.org>
2018-11-05 02:16:20 -0800

    Added employment record validation to ensure end date is greater than start date

--- app/models/employment_record.rb
+++ app/models/employment_record.rb
@@ -2,9 +2,17 @@ class EmploymentRecord < ApplicationRecord
   belongs_to :user
 
   validates :employer_name, :start_date, :job_title, :job_description, :sort_order, presence: true
+  validate :end_date_greater_than_start_date
 
   def format_end_date
     return self.end_date.strftime("%B %Y") if self.end_date.present?
     'Present'
+  end
+
+  def end_date_greater_than_start_date
+    return if [start_date.blank?, end_date.blank?].any?
+    if end_date < start_date
+      errors.add(:end_date, 'must be greater than start date')
+    end
   end
 end
